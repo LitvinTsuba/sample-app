d21f2bce4dbedfd1e9137a2c8240c27f42d0f910
Andrey Tsuba <garde4@gmail.com>
2020-05-25 11:14:05 +0300

    Implement basic login

--- db/schema.rb
+++ db/schema.rb
@@ -1,6 +1,9 @@
+# frozen_string_literal: true
+
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
-# incrementally modify your database, and then regenerate this schema definition.
+# incrementally modify your database, and then regenerate this schema
+# definition.
 #
 # This file is the source Rails uses to define your schema when running `rails
 # db:schema:load`. When creating a new database, `rails db:schema:load` tends to
@@ -8,17 +11,16 @@
 # migrations from scratch. Old migrations may fail to apply correctly if those
 # migrations use external dependencies or application code.
 #
-# It's strongly recommended that you check this file into your version control system.
+# It's strongly recommended that you check this file into your version
+# control system.
 
-ActiveRecord::Schema.define(version: 2020_05_14_085404) do
-
-  create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "email"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.string "password_digest"
-    t.index ["email"], name: "index_users_on_email", unique: true
+ActiveRecord::Schema.define(version: 20_200_514_085_404) do
+  create_table 'users', force: :cascade do |t|
+    t.string 'name'
+    t.string 'email'
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.string 'password_digest'
+    t.index ['email'], name: 'index_users_on_email', unique: true
   end
-
 end
