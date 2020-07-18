e02940b7d8e83adbbe13b55911de010b3c5b4270
Andrey Tsuba <garde4@gmail.com>
2020-07-18 19:42:38 +0300

    Add password reset mailer and templates

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class PasswordResetsController < ApplicationController
+  def edit; end
+
   def new; end
 
   def create
@@ -15,6 +17,4 @@ class PasswordResetsController < ApplicationController
       render 'new'
     end
   end
-
-  def edit; end
 end
