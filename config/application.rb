43b226177a556157462a6438725a1852ea442da0
Andrey Tsuba <garde4@gmail.com>
2020-11-06 23:20:39 +0300

    Add ajax for follow/unfollow button.

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,7 @@ module SampleApp
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
-  end
+    # the framework and sany gems in your application.
+    # Include the authenticity token in remote forms.
+    config.action_view.embed_authenticity_token_in_remote_forms = true end
 end
